93e62c6f31fc15e4ccf358ab23688b9624675ecd
Jonathan J <iksose.pt3@gmail.com>
2013-05-08 16:26:14 -0500

    Removed insecure, and allowed certain writes to posts

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,22 @@
+Template.postSubmit.events({
+  'submit form': function(event) {
+    event.preventDefault();
+
+    // var post = {
+    //   url: $(event.target).find('[name=url]').val(),
+    //   title: $(event.target).find('[name=title]').val(),
+    //   message: $(event.target).find('[name=message]').val()
+    // }
+
+    var post = {
+    	url: $("#url").val(), 
+    	title: $("#title").val(),
+    	message: $("#message").val()
+    }
+
+   
+
+    post._id = Posts.insert(post);
+    Meteor.Router.to('postPage', post);
+  }
+});
